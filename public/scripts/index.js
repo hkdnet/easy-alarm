728bdf6d92c05389974252764861723837d03d5c
hkdnet <hkdnet@users.noreply.github.com>
2015-07-15 02:01:15 +0900

    optimize deleting

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,18 +10,19 @@ $(function() {
     var $list = $('#alarm-list');
     var today = new Date();
     $list.append(function() {
-      return $.map(arr, function(e) {
-        var text = e.date + " " + e.hours + ":" + e.minutes + " [" + e.title + "] " + e.msg;
+      var $lis = $.map(arr, function(e) {
         var target = converter(e);
         if(target > today) {
+          var text = e.date + " " + e.hours + ":" + e.minutes + " [" + e.title + "] " + e.msg;
           return $('<li>').text(text);
         } else {
           $.ajax({
             type: "DELETE",
             url: "/alarms/" + e.id
-          })
+          });
         }
       });
+      return $lis;
     });
   });
 });
